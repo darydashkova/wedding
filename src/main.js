80567ac3e4032f592ca46bc11fde64bbd83653f5
Daria Sitlivaya <sde@reputation.house>
2026-03-19 15:28:43 +0300

    front(feat): шрифты

--- src/main.js
+++ src/main.js
@@ -2,4 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './styles/main.css'
 
+const root = document.documentElement
+
+const markFontsReady = () => {
+  root.classList.remove('fonts-loading')
+  root.classList.add('fonts-ready')
+}
+
+if (document.fonts?.ready) {
+  document.fonts.ready.then(markFontsReady).catch(markFontsReady)
+} else {
+  markFontsReady()
+}
+
 createApp(App).mount('#app')
